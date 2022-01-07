277dcce0de9cb8ba4fe28908eb6087d8e02e6615
Nilgun <94299742+nilgunaydin@users.noreply.github.com>
2022-01-07 14:15:13 +0300

    project2done

--- src/GaleriOtomasyon/Islemler.java
+++ src/GaleriOtomasyon/Islemler.java
@@ -1,37 +1,52 @@
 package GaleriOtomasyon;
 
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+
+import java.util.*;
 
 public class Islemler {
 
 static DenemeAracVeri arac = new DenemeAracVeri();
  public static void odeme() {
-  System.out.println("Galerideki Mevcut Araclarimiz");
-  arac.aracListesi();
+ System.out.println("Galerideki Mevcut Araclarimiz");
+ arac.aracListesi();
   Scanner scan = new Scanner(System.in);
+
   int secim;
-  do{
-   System.out.println("Secmek istediginiz Arac Noyu(10,11,12,13,14) Giriniz");
+
+  System.out.println("Galerideki Mevcut Araclarimiz");
+  System.out.println("Secmek istediginiz Arac Noyu Giriniz");
+  secim = scan.nextInt();
+  while (!arac.aracMap.containsKey(secim)) {
    secim = scan.nextInt();
-  // System.out.println("agam sectigin arac bizde yok 10-13 arasi gir");
-  }while (!arac.aracMap.containsKey(secim));
-
+   System.out.println("agam sectigin arac bizde yok 10-13 arasi gir");
+  }
   System.out.println("sectigin arac : " + arac.araclar(secim));
+  Integer borc = Integer.valueOf(arac.araclar(secim).get("Fiyat"));
+  System.out.println("odemeniz gereken tutar: "+ arac.araclar(secim).get("Fiyat")+"\n"+
+          "lutfen odeyeceginiz tutari giriniz:");
 
+int odenen;
+  do {odenen=scan.nextInt();
+   borc-=odenen;
+   if(borc>0){
+    System.out.println("agam yeterli degil eksik kalan : "+ borc);
+   }else if(borc<0){
+    System.out.println("para ustunuz:" + -1*borc +" tl");
+   break;
+   }else{break;
+   }
+  } while (true);
+
+  System.out.println("odemeniz alindi :)");
  }
 }
- /*
-  System.out.println("Galerideki Mevcut Araclarimiz");
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Secmek istediginiz Arac Noyu Giriniz");
-        secim = scan.nextInt();
-        while (!arac.aracMap.containsKey(secim)) {
-            secim = scan.nextInt();
-            System.out.println("agam sectigin arac bizde yok 10-13 arasi gir");
-        }
+ /* do{
+   System.out.println("Secmek istediginiz Arac Noyu(10,11,12,13,14) Giriniz");
+   secim = scan.nextInt();
+  System.out.println("agam sectigin arac bizde yok 10-13 arasi gir");
+  }while (!arac.aracMap.containsKey(secim));
+
+
         System.out.println("sectigin arac : " + arac.araclar(secim));
    */
   /*
--- src/GaleriOtomasyon/Main.java
+++ src/GaleriOtomasyon/Main.java
@@ -5,15 +5,12 @@ import java.util.Scanner;
 public class Main {
     static DenemeAracVeri calis = new DenemeAracVeri();
     public static void main(String[] args) {
-
+        System.out.println("Toyota Galeriye Hosgeldiniz");
         galeriMenu();
-
-
     }
     public static void galeriMenu() {
 
-        System.out.println("Toyota Galeriye Hosgeldiniz");
-        System.out.println("1==>Arac Listesi\n2==>Satin Alma\n3==>Cikis");
+        System.out.println("\t\t======== MENU ======= \n\t\t1==>Arac Listesi\n\t\t2==>Satin Alma\n\t\t3==>Cikis");
         Scanner scan = new Scanner(System.in);
         int tercih = scan.nextInt();
 
@@ -25,13 +22,15 @@ public class Main {
             case 2:
               Islemler islemler = new Islemler();
               Islemler.odeme();
-
+                galeriMenu();
                 break;
             case 3:
+                System.out.println("yine bekleriz...");
                System.exit(0);
                 break;
             default :{
                 System.out.println("Hatali Giris Yaptiniz");
+                galeriMenu();
             }
         }
 
